3450b71c5f076458e5787520a416ac66b6cae56a
gongahkia <angryapplegravy@gmail.com>
2024-03-29 01:04:58 +0800

    add more soon

---
+++ clamp.lua
@@ -0,0 +1,6 @@
+-- @param specified value to be clamped, minimum value of the clamp range, maximum value of the clamp range
+-- @return clamped value within the specified range
+
+function clamp(value, min, max)
+    return math.min(math.max(value, min), max)
+end
---
+++ manhattanDistance.lua
@@ -0,0 +1,6 @@
+-- @param entity1 table with coordinates {x,y}, entity2 table with coordinates {x,y}
+-- @return manhattan distance between coordinates of both entities
+
+function manhattanDistance(ety1, ety2)
+    return math.abs(ety2[1] - ety1[1]) + math.abs(ety2[2] - ety1[2])
+end
